f1d550044654220172edad6cfd1158538c97f761
Vivian Azalia <vivianazalia>
2020-09-02 01:14:34 +0700

    Adding Asset to Scene Unity
    Adding Background, Player, Ground, Script ControlPlayer but not be finished.

---
+++ Platformer 2D/Assets/Scripts/ControlPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPlayer : MonoBehaviour
+{
+    Rigidbody2D playerRb;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //Player jalan ke kanan
+        } 
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            //Player jalan ke kiri
+        }
+        else
+        {
+            //Animasi Idle
+        }
+        
+    }
+}
